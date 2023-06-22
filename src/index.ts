9e74607f0942fe4c78d98c6fb37278a58064679d
tomoda <tomoda@rook-jp.com>
2023-06-22 22:29:17 +0900

    refactor: 型名を変更

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { ratio, Ratio } from './Ratio';
 import { type ConvertableToBigInt, type ConvertableToRatio } from './types';
 
-export { ratio, Ratio, type ConvertableToBigInt, type ConvertableToRatio as ConvertableToRational };
+export { ratio, Ratio, type ConvertableToBigInt, type ConvertableToRatio };
